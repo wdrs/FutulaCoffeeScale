4dff4f8c01985fc6ce9227515a1eb5472eb2e103
wdrs <ya.dolzen.bit.tut@gmail.com>
2025-07-24 14:03:04 +0300

    Updated AGP to 8.11.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 6
-        versionName = "1.1.3"
+        versionName = "1.1.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     ksp {
