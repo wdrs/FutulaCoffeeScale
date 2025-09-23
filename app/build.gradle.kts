c725c92d9c1043402c74a145776f4477ea2b5d8c
metya <metya.tm@gmail.com>
2025-09-24 00:27:42 +0300

    Bump version of app and code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.tomatishe.futulacoffeescale"
         minSdk = 29
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.1.3"
+        versionCode = 7
+        versionName = "1.1.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     ksp {
